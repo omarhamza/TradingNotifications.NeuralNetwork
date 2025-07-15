6a54dc4f9578c0ba0600e1e1733a7c469245a2c2
Omar HAMZA <omarhamza37@gmail.com>
2025-07-15 20:04:21 +0200

    feat: send telegram messages

---
+++ src/send_telegram_message.py
@@ -0,0 +1,18 @@
+import requests
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
+def notify(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    title = "--*Neural network*--"
+    full_message = f"{title}\n{message}"
+
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": full_message,
+        "parse_mode": "Markdown"
+    }
+    try:
+        requests.post(url, data=payload)
+    except Exception as e:
+        print("Erreur envoi Telegram :", e)
