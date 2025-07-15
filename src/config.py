c166b797360db0729263e4514cce8e5d051232c1
Omar HAMZA <omarhamza37@gmail.com>
2025-07-15 20:03:11 +0200

    feat: update config file

---
+++ src/config.py
@@ -0,0 +1,19 @@
+# Configuration
+SYMBOL = 'BTCUSDT'
+SYMBOLS = ['BTC/USDT', 'ETH/USDT', 'SOL/USDT']
+SEQ_LEN = 50
+SLEEP_TIME = 15
+TIMEFRAME = '1h'
+MAX_DAYS=365
+API_URL = "https://api.binance.com/api/v3/klines"
+TELEGRAM_TOKEN = "7706670085:AAGRMve7EuhFo1i8C2U22JdNPyGyvjN4-N8"
+TELEGRAM_CHAT_ID = "7664939619"
+
+INTERVAL = "15m"
+MODEL_PATH = "crypto_model.keras"
+
+features = [
+    'rsi', 'rsi_delta', 'macd', 'macd_signal',
+    'ema_20', 'ema_50', 'bb_high', 'bb_low',
+    'obv', 'volatility', 'stoch_k', 'stoch_d'
+]
