a8b1a350254aa39099251dad87cafd241bb70d69
Omar HAMZA <omarhamza37@gmail.com>
2025-07-17 14:06:19 +0200

    feat: check sell signal

---
+++ src/entities/Prediction.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Prediction(Enum):    
+    BUY = "buy"
+    SELL = "sell"
+    NONE = "none"
--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,16 @@
+from entities.Prediction import Prediction
 from utils import log
 
 def predict(model, X):
     prediction = model.predict(X)
-    shouldIBuyCrypto = float(prediction[0][0])
-    log(f"prediction-----------: {shouldIBuyCrypto:.2f}")
+    prediction_score = float(prediction[0][0])
+    log(f"prediction-----------: {prediction_score:.2f}")
 
     log("keras--------------------")
     print(model.summary())
     
-    return prediction[0][0] >= 0.5
+    return (
+        Prediction.BUY if prediction_score > 0.6 else
+        Prediction.SELL if prediction_score < 0.2 else
+        Prediction.NONE
+    )
